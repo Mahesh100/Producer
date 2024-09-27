726885c2010cdbdee93c8689de5316ff54a2874d
Mahesh Kakde <maheshkakde100@gmail.com>
2024-09-27 18:40:56 +0530

    Controller and Service added

---
+++ src/main/java/com/codedecode/kafka/producer/Producer/controller/producerController.java
@@ -0,0 +1,7 @@
+package com.codedecode.kafka.producer.Producer.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class producerController {
+}
---
+++ src/main/java/com/codedecode/kafka/producer/Producer/service/producerService.java
@@ -0,0 +1,7 @@
+package com.codedecode.kafka.producer.Producer.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class producerService {
+}
